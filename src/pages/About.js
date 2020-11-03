82e972a8b6617ae74b5777e3a7721b404eef5be4
Lauren Brown <laurenbrown108@gmail.com>
2020-11-03 09:26:22 -0500

    about styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,12 +9,12 @@ function About() {
                     <section className="col-md-8">
                         <h1>About</h1>
                         <section className="wrapper pt-2">
-                            <img src="../../assets/img_0018_2.jpg" className="rounded float-left pt-2 pr-4" alt="Lauren" />
-                            <section className="pt-3">
+                            <img src="/assets/img_0018_2.jpg" className="rounded float-left mt-2 mr-4 mb-2 border-dark" alt="Lauren" />
+                            <section className="pt-2">
                                 <p>Hello, and welcome to my portfolio!</p>
                                 <p>My name is Lauren. I'm from Cherry Hill, NJ and currently live in Philadelphia with my two dogs, Cowboy and Sugar.</p>
-                                <p>I graduated from Rutgers University in 2014 with a Bachelor's degree in Journalism and Media Studies. For the past six years I have mostly been working in retail and customer service, and I'm 100% ready for a career change. </p>
-                                <p>I'm a beginner when it comes to web development, so I'm excited to rapidly develop my skills. The content I add to this portfolio will only improve over time! If you like what you see, you can connect with me on social media, or fill out the contact form provided.</p>
+                                <p>I graduated from Rutgers University in 2014 with a Bachelor's degree in Journalism and Media Studies. For the past six years I have mostly been working in customer service, but I've also dabbled in visual merchandising and styling. One of my true passions is definitely design, whether that be in fashion, print, or on the web.</p>
+                                <p>I'm a beginner when it comes to web development, so the content I add to this portfolio will only improve over time! If you like what you see please connect with me on social media.</p>
                             </section>
                         </section>
                     </section>
