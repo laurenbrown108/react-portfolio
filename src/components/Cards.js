8ab33c912d1ca54b147b1b40b1488d3a97a96f2a
Lauren Brown <laurenbrown108@gmail.com>
2020-11-04 08:42:59 -0500

    cards mapping on portfolio. Incomplete styling.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,24 +2,25 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 
-function PortfolioCard() {
+function PortfolioCard(props) {
+    console.log(props)
     return (
         <>
 
 
-            <div className="col-md-12 ml-4 pl-4">
-                <Row>
+            {/* <div className="col-md-12 ml-4 pl-4"> */}
+                
                     <Card className="mt-2 mr-3 border-dark pCard">
-                        <Card.Img src="assets/brewfinder.gif" className="card-img-top img-fluid" alt="Brewery finder demo" />
+                        <Card.Img src={props.image} className="card-img-top img-fluid" alt={props.title} />
                         <Card.Body>
-                            <Card.Title className="cardTitle"> Brewery Finder </Card.Title>
-                            <Card.Text className="cardBody"> An app that lets the user search for breweries by state. It utilizes the Open Brewery DB, Yelp, and Google Maps APIs. </Card.Text>
-                            <a href="https://github.com/laurenbrown108/Brewery-Finder" target="_blank" rel="noreferrer" className="btn mb-1 btn-primary"> Github Repo</a>
-                            <a href="https://lambaa94.github.io/Brewery-Finder/" target="_blank" rel="noreferrer" className="btn mb-1 btn-primary"> Deployed App </a>
+                            <Card.Title className="cardTitle"> {props.title} </Card.Title>
+                            <Card.Text className="cardBody"> {props.description} </Card.Text>
+                            <a href={props.githubURL} target="_blank" rel="noreferrer" className="btn mb-1 btn-primary"> Github Repo</a>
+                            <a href={props.exampleURL} target="_blank" rel="noreferrer" className="btn mb-1 btn-primary"> Deployed App </a>
                         </Card.Body>
                     </Card>
-                </Row>
-            </div>
+                
+            {/* </div> */}
 
         </>
     )
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import examples from '../examples.json'
+import examples from '../examples.json'
 import PortfolioCard from '../components/Cards';
 import Row from 'react-bootstrap/Row';
 // import Card from 'react-bootstrap/Card';
@@ -8,10 +8,23 @@ import Container from 'react-bootstrap/Container'
 function Portfolio() {
     return (
         <>
-            <Container className="py-5 mb-5">
+            <Container className="py-5 mb-5 ml-4">
                     <h1> PORTFOLIO </h1>
-                    <Row>
-                        <PortfolioCard />
+                    <Row className="pt-2 pl-5">
+                        {console.log(examples)}
+                        {examples.map(example =>(
+                        <PortfolioCard
+                            key={example.id}
+                            title={example.title}
+                            image={example.image}
+                            description={example.description}
+                            exampleURL={example.exampleURL}
+                            githubURL={example.githubURL}
+
+
+                            
+                        />
+                        ))}
                     </Row>
             </Container>
            
