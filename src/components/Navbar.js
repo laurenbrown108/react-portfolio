bd8a913966f21519871a04456c244c450e6bdddb
Lauren Brown <laurenbrown108@gmail.com>
2020-11-04 21:39:55 -0500

    nav collapsing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,19 @@ function MyNav() {
     return (
         <>
             <header>
-                <Navbar className="shadow pt-2 pb-2 navbar bg-light navbar-light">
+                <Navbar collapseOnSelect expand="lg" className="shadow pt-2 pb-2 navbar bg-light navbar-light">
                     <Navbar.Brand className="name" href="/about"> LAUREN BROWN</Navbar.Brand>
-                    <Nav className="ml-auto navlink">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto navlink">
 
-                        <Nav.Link href="/about">ABOUT</Nav.Link>
+                            <Nav.Link href="/about">ABOUT</Nav.Link>
 
-                        <Nav.Link href="/portfolio">PORTFOLIO</Nav.Link>
+                            <Nav.Link href="/portfolio">PORTFOLIO</Nav.Link>
 
 
-                    </Nav>
-
+                        </Nav>
+                    </Navbar.Collapse>
 
                 </Navbar>
 
